d9c226f1a4495f37499a1d3515316d73fd42b7b9
Anonymous <flu2020@pm.me>
2020-08-28 13:52:17 +0300

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='core-service',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/sarafanio/core-service.git',
     author='Sarafan Community',
     author_email='flu2020@pm.me',
